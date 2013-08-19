bad676a041839dbc5271d2edba36a4c529014099
Alfredo Arias <alfre.arias@gmail.com>
2013-08-19 15:20:20 -0400

    deploy for heroku

---
+++ web.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var fs = require('fs');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  var buf = fs.readFileSync('index.html', 'utf-8');
+  response.send(buf.toString('utf-8',0,buf.length));
+});
+
+var port = process.env.PORT || 8080;
+
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
